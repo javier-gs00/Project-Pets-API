674b9b00e8349334287888951fa28499798109a2
Javier Gutiérrez <28971285+javier-gs00@users.noreply.github.com>
2017-12-03 20:05:31 -0300

    Minor fixes

--- server.js
+++ server.js
@@ -1,44 +1,34 @@
-const express = require('express');
-const path = require('path');
-const routes = require('./controllers/index');
-const mongoose = require('mongoose');
+const express = require('express')
+const path = require('path')
+const routes = require('./controllers/index')
+const mongoose = require('mongoose')
 
-const app = express();
-require('dotenv').config();
+const app = express()
+require('dotenv').config()
 
 // Set global var
-global.__rootDir = __dirname;
+global.__rootDir = __dirname
 
 // MongoDB setup
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/projectpets', {
     useMongoClient: true
-});
+}, (err) => {
+    if (err) console.log('Err connecting to the db.. err desc=', err)
+    console.log('Connection to database succesful')
+})
 const db = mongoose.connection
-db.on('error', console.error.bind(console, 'db connection error: '));
+db.on('error', console.error.bind(console, 'db connection error: '))
 
-app.set('port', (process.env.PORT || 3001));
-app.use('/api', routes);
+app.set('port', (process.env.PORT || 3001))
+app.use('/api', routes)
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-// Catch 404 and forward to error handler
-// app.use((req, res, next) => {
-//     let err = new Error('Not found');
-//     err.status = 404;
-//     res.send("Error 404: File not found...")
-//     next(err);
-// })
 //Basic error-handling middleware
 app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).send(err.response || 'Something broke :(...');
+    console.log(err)
+    res.status(500).send(err.response || 'Something broke :(...')
 })
 
-// app.listen(process.env.PORT || 3001, () => {
-//     const port = process.env.PORT;
-//     console.log(`App listening on port ${port}`);
-// })
 app.listen(app.get('port'), () => {
-    console.log('App listening on port', app.get('port'));
+    console.log('App listening on port', app.get('port'))
 })
