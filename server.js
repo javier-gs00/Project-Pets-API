58c86e6000568584cf715b220de83afa53052453
Javier Gutiérrez <28971285+javier-gs00@users.noreply.github.com>
2017-12-04 15:59:40 -0300

    Added CORS support

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const path = require('path')
 const routes = require('./controllers/index')
 const mongoose = require('mongoose')
@@ -20,7 +21,19 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/projectpets', {
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'db connection error: '))
 
+// Set PORT and CORS configuration 
 app.set('port', (process.env.PORT || 3001))
+const whitelist = ['https://project-pets-client.herokuapp.com/']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+app.use(cors(corsOptions))
 app.use('/api', routes)
 
 //Basic error-handling middleware
