dd45549cbac0d58e723ae2a1da4bfef5852822ae
Javier Gutiérrez <28971285+javier-gs00@users.noreply.github.com>
2017-12-04 22:30:03 -0300

    Removed CORS options

--- server.js
+++ server.js
@@ -41,29 +41,29 @@ app.set('port', (process.env.PORT || 3001))
 // app.use(cors(corsOptions))
 
 // Add headers
-app.use(function (req, res, next) {
-    console.log('======= REQUEST HEADERS ========')
-    console.log(req.header)
+// app.use(function (req, res, next) {
+//     console.log('======= REQUEST HEADERS ========')
+//     console.log(req.header)
     
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://project-pets-client.herokuapp.com/');
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'https://project-pets-client.herokuapp.com/');
+//     // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', false);
 
-    console.log('======= RESPONSE HEADERS ========')
-    console.log(res.headers)
-    // Pass to next layer of middleware
-    next();
-});
+//     console.log('======= RESPONSE HEADERS ========')
+//     console.log(res.headers)
+//     // Pass to next layer of middleware
+//     next();
+// });
 
 app.use('/api', routes)
 
