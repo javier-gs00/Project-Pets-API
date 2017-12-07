7af96234964e0f6f3c5c55e10ee3c66ddac33f4f
Javier Gutiérrez <28971285+javier-gs00@users.noreply.github.com>
2017-12-07 12:38:22 -0300

    Added Pet Happy dog & cat accessories scrapers

---
+++ utils/web_scrapers/accessory/pethappy_cat.js
@@ -0,0 +1,56 @@
+const xray = require('x-ray')
+const timer = require('../../timer')
+const latinUnicodeParser = require('../../latinUnicodeParser')
+
+let x  = xray({
+    filters: {
+        parseHexUnicode: function (value) {
+            return value.indexOf('&#') > 0 ? latinUnicodeParser(value) : value
+        },
+        priceToInt: function (value) {
+            return typeof value == 'string' ? parseInt(value.replace(/[A-z$,.]/g, '')) : value;
+        },
+        storeName: function () {
+            return 'Pet Happy'
+        },
+        category: function () {
+            return 'accessory'
+        },
+        animal: function () {
+            return 'cat'
+        }
+    }
+}).delay(1000);
+
+module.exports = () => {
+    return new Promise ((resolve, reject) => {
+        console.log('==== Starting Pet Happy cat accessory web scraper ====')
+        let startTimer = timer();
+        // Init scraper
+        x(
+            'https://www.pethappy.cl/gatos-2/accesorios',
+            'div.large-p.columns.producto-box',
+            [{
+                name: 'h1 a@html | parseHexUnicode',
+                href: 'a@href',
+                price: '.precio | priceToInt',
+                imageHref: 'img@src',
+                store: 'h1 | storeName',
+                category: 'h1 | category',
+                animal: 'h1 | animal'
+            }]
+        )
+        .paginate('li.next a@href') // Next page button .css classes
+        .limit(6) // Pages to crawl limit
+        ((err, data) => {
+            if (err) {
+                console.log('Error fron Pet Happy cat accessory web scraper...')
+                reject(err)
+            } else {
+                let endTimer = timer(startTimer)
+                console.log("== Pet Happy cat accessory web scraper completed in: " + endTimer + " ms ==")
+                resolve(data)
+            }
+        })
+    })
+}
